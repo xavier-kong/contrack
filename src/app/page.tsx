293121f565b14b735ea16f88fc0d4a40d7c5b075
xavier-kong <kongxianchengxavier@gmail.com>
2024-06-23 14:39:29 +0800

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,9 @@ function TopBottomTextLayout({ upperText, lowerText }: { upperText: string; lowe
 
 export default async function Home() {
   return (
-    <div className="bg-slate-950 flex flex-1 h-screen flex-row">
+    <div className="bg-slate-950 flex flex-1 h-screen flex-row p-20">
       <div className="flex flex-col w-2/5 text-white align-middle h-full border-blue-100 border-2 justify-center">
-        <div className="flex-1">
-          <p>Hello! John Doe</p>
-        </div>
-        <div className="border-white border-2 rounded flex-1 flex-col flex pl-5">
+        <div className="rounded flex-1 flex-col flex pl-5">
           <div className="flex-1 justify-start items-end flex p-1">
             <p className="text-2xl">
               Events
@@ -25,20 +22,60 @@ export default async function Home() {
             </p>
           </div>
         </div>
-        <div className="border-white border-2 rounded flex-1">
-          Flights
-        </div>
-        <div className="flex flex-row flex-1">
-          <div className="border-white border-2 rounded flex-1">
-            Flight Time
+
+        <div className="rounded flex-1 flex-col flex pl-5">
+          <div className="flex-1 justify-start items-end flex p-1">
+            <p className="text-xl">
+              Countries
+            </p>
           </div>
-          <div className="border-white border-2 rounded flex-1">
-            Flight Distance
+          <div className="flex-1 justify-start items-start flex p-1">
+            <p className="text-5xl font-bold">
+              5
+            </p>
           </div>
         </div>
-        <div></div>
+
+        <div className="rounded flex-1 flex-col flex pl-5">
+          <div className="flex-1 justify-start items-end flex p-1">
+            <p className="text-2xl">
+              Flights
+            </p>
+          </div>
+          <div className="flex-1 justify-start items-start flex p-1">
+            <p className="text-5xl font-bold">
+              5
+            </p>
+          </div>
+        </div>
+
+        <div className="rounded flex-1 flex-col flex pl-5">
+          <div className="flex-1 justify-start items-end flex p-1">
+            <p className="text-xl">
+              Flight Time
+            </p>
+          </div>
+          <div className="flex-1 justify-start items-start flex p-1">
+            <p className="text-5xl font-bold">
+              5 mins
+            </p>
+          </div>
+        </div>
+
+        <div className="rounded flex-1 flex-col flex pl-5">
+          <div className="flex-1 justify-start items-end flex p-1">
+            <p className="text-xl">
+              Flight Distance
+            </p>
+          </div>
+          <div className="flex-1 justify-start items-start flex p-1">
+            <p className="text-5xl font-bold">
+              5km
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 bg-green-900">
         Globe
       </div>
     </div>
