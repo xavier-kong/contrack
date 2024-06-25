08ac99c9628db9c58dfb8b00aa12b9600c29dcb9
xavier-kong <kongxianchengxavier@gmail.com>
2024-06-25 21:44:23 +0800

    globe for later ig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,6 @@
-import Globe from "react-globe.gl";
-import * as d3 from "d3";
 
-function TopBottomTextLayout({ upperText, lowerText }: { upperText: string; lowerText: string; }) {
-  return (
-    <div className="border-white border-2 rounded flex-1">
-
-    </div>
-  );
-}
-
-async function fetchData() {
-  const airportParse = ([airportId, name, city, country, iata, icao, lat, lng, alt, timezone, dst, tz, type, source]) => ({ airportId, name, city, country, iata, icao, lat, lng, alt, timezone, dst, tz, type, source });
-  const routeParse = ([airline, airlineId, srcIata, srcAirportId, dstIata, dstAirportId, codeshare, stops, equipment]) => ({ airline, airlineId, srcIata, srcAirportId, dstIata, dstAirportId, codeshare, stops, equipment});
-
-  const [airportData, routes] = await Promise.all([
-    fetch('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat').then(res => res.text())
-    .then(d => d3.csvParseRows(d, airportParse)),
-    fetch('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat').then(res => res.text())
-    .then(d => d3.csvParseRows(d, routeParse))
-  ])
-
-  const byIata = indexBy(airports, 'iata', false);
-
-  const routeData = routes
-  .filter(d => byIata.hasOwnProperty(d.srcIata) && byIata.hasOwnProperty(d.dstIata)) // exclude unknown airports
-  .filter(d => d.stops === '0') // non-stop flights only
-  .map(d => Object.assign(d, {
-    srcAirport: byIata[d.srcIata],
-    dstAirport: byIata[d.dstIata]
-  }))
-  .filter(d => d.srcAirport.country === COUNTRY && d.dstAirport.country !== COUNTRY); // international routes from country
-
-  return { airportData, routeData };
-}
 
 export default async function Home() {
-
-  const { airportData, routeData } = await fetchData();
-
   return (
     <div className="bg-slate-950 flex flex-1 h-screen flex-row p-20">
       <div className="flex flex-col w-2/5 text-white align-middle h-full border-blue-100 border-2 justify-center">
@@ -105,29 +68,6 @@ export default async function Home() {
             </p>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col flex-1 bg-green-900">
-        <Globe 
-          globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-          arcsData={routeData}
-          arcLabel={d => `${d.airline}: ${d.srcIata} &#8594; ${d.dstIata}`}
-          arcStartLat={d => +d.srcAirport.lat}
-          arcStartLng={d => +d.srcAirport.lng}
-          arcEndLat={d => +d.dstAirport.lat}
-          arcEndLng={d => +d.dstAirport.lng}
-          arcDashLength={0.25}
-          arcDashGap={1}
-          arcDashInitialGap={() => Math.random()}
-          arcDashAnimateTime={4000}
-          arcColor={d => [`rgba(0, 255, 0, ${OPACITY})`, `rgba(255, 0, 0, ${OPACITY})`]}
-          arcsTransitionDuration={0}
-
-          pointsData={airportData}
-          pointColor={() => 'orange'}
-          pointAltitude={0}
-          pointRadius={0.02}
-          pointsMerge={true}
-        />
       </div>
     </div>
   );
